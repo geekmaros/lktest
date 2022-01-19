89b0502f8a343a55cbed47783ba3461221798d6a
amustapha <amustapha@linkfire.com>
2022-01-19 13:20:24 +0100

    added stuffs

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import i18n from './i18n'
 
 Vue.config.productionTip = false;
 
 new Vue({
+  i18n,
   router,
   store,
   render: (h) => h(App),
